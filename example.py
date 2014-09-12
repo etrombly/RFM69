2ba61c9cb85cf74ef398933fa021ca3a361da991
Eric Trombly <etrombly@yahoo.com>
2014-09-12 15:24:25 +0200

    added example script

---
+++ example.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import RFM69
+from RFM69registers import *
+
+test = RFM69.RFM69(RF69_915MHZ, 0x01, 0x01, True)
+print "class initialized"
+print "reading all registers"
+results = test.readAllRegs()
+for result in results:
+  print result
+print "Performing rcCalibration"
+test.rcCalibration()
+print "setting high power"
+test.setHighPower(True)
+print "Checking temperature"
+print test.readTemperature(0)
+print "sending blah to 0x02"
+test.send(0x02, "blah", False)
+print "setting encryption"
+test.encrypt("1234567891011121")
+print "sending blah to 0x03"
+test.send(0x03, "blah", True)
+print "shutting down"
+test.shutdown()
