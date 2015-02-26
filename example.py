3585946b398758bb8ba0afb998a1c1925bf7400f
Eric Trombly <etrombly@gmail.com>
2015-02-26 01:58:33 +0000

    fixed sendACK, added setNetwork. Everything is functioning correctly now

--- example.py
+++ example.py
@@ -17,26 +17,19 @@ print "setting high power"
 test.setHighPower(True)
 print "Checking temperature"
 print test.readTemperature(0)
+print "setting encryption"
+test.encrypt("1234567891011121")
 print "sending blah to 2"
 if test.sendWithRetry(2, "blah", 3, 20):
     print "ack recieved"
-#print "setting encryption"
-#test.encrypt("123456789101112")
-#print "sending blah to 2"
-#if test.sendWithRetry(2, "blah"):
-#    print "ack recieved"
 print "reading"
-while False:
+while True:
     test.receiveBegin()
     while not test.receiveDone():
         pass
-    recieved_time = datetime.datetime.now()
     if test.ACKRequested():
         print "sending ack"
-        test.sendACK("ok")
-        sent_time = datetime.datetime.now()
-        elapsed_time = sent_time - recieved_time
-        print "responded in %s milliseconds" % (elapsed_time.total_seconds() * 1000,)
+        test.sendACK()
     print "".join([chr(letter) for letter in test.DATA])
 print "shutting down"
 test.shutdown()
